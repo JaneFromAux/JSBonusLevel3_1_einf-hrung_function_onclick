874105b645070f8918f7567cd344c2d142853334
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-20 14:33:41 +0200

    Aufgabe erledigt

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+let headline = document.getElementById('headline');
+
+function changeBgAndTextColor1() {
+    console.log('function runs');
+    document.body.style.backgroundColor = 'green';
+    headline.style.color = 'purple';
+}
+
+function changeBgAndTextColor2() {
+    console.log('function runs');
+    document.body.style.backgroundColor = 'darkslateblue';
+    headline.style.color = 'gold';
+}
+
+function changeBgAndTextColor3() {
+    console.log('function runs');
+    document.body.style.backgroundColor = 'black';
+    headline.style.color = 'orange';
+}
+function changeBgAndTextColor4() {
+    console.log('function runs');
+    document.body.style.backgroundColor = 'navy';
+    headline.style.color = 'aqua';
+}
